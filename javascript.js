6a26a79f05d5d506b59f7fd9f46923365d6e4efc
unknown <theo.paste@depinfoad.univ-brest.fr>
2023-01-11 08:18:05 +0100

    objet aPerson

--- javascript.js
+++ javascript.js
@@ -4,3 +4,16 @@ const a = 12;
 const b = 45;
 b+=a;
 console.log("result = "+b);
+
+
+// on peut également déclarer et initialiser un objet de la façon suivante :
+
+let aPerson= new Object();
+aPerson.name = 'Jean';
+aPerson.age = 35;
+aPerson.gender = 'masculin';
+aPerson.interest = ['musique', 'badminton'];
+
+aPerson.sayHello = function() {console.log('Bonjour ! Je suis ' + this.name + '.'); };
+
+aPerson.sayHello();
